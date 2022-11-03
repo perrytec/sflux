688fe2079bcda81ded74484e80579ce703ea2d33
philip <philip.liedtke@perry.sg>
2022-11-03 17:57:04 +0000

    Implements not equal

--- src/sflux/utils.py
+++ src/sflux/utils.py
@@ -6,6 +6,9 @@ class ROW:
     def __eq__(self, other: str):
         return f'r["{self.column}"] == "{other}"'
 
+    def __ne__(self, other):
+        return f'r["{self.column}"] != "{other}"'
+
     def __ge__(self, other):
         return f'r["{self.column}"] >= "{other}"'
 
